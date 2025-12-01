9ce085df9368857b0be3d307aceecba77100b67a
Oskar Anderson <oskaranderson99@gmail.com>
2025-12-01 11:17:05 +0100

    Rust: Updated template project
    Fixed incorrect name for test data file: tesdata.txt -> testdata.txt
    Added calculate_small_test using the smalltestdata.txt data file.
    Added empty data files: data.txt, testdata.txt, smalltestdata.txt
    
    Copied changes to 2025/day_01

--- Rust/2025/day_01/src/main.rs
+++ Rust/2025/day_01/src/main.rs
@@ -28,10 +28,23 @@ fn main() {
 #[test]
 fn calculate_test() {
     let expected_value = 0;
-    match calculate("tesdata.txt") {
+    match calculate("testdata.txt") {
         Ok(value) => assert_eq!(
             value, expected_value,
-            "Program finished but result was wrong! Expected: {} but received: {}",
+            "Program using testdata.txt finished but result was wrong! Expected: {} but received: {}",
+            expected_value, value
+        ),
+        Err(err) => panic!("Error occured:\n{}", err),
+    }
+}
+
+#[test]
+fn calculate_small_test() {
+    let expected_value = 0;
+    match calculate("smalltestdata.txt") {
+        Ok(value) => assert_eq!(
+            value, expected_value,
+            "Program using smalltestdata.txt finished but result was wrong! Expected: {} but received: {}",
             expected_value, value
         ),
         Err(err) => panic!("Error occured:\n{}", err),
