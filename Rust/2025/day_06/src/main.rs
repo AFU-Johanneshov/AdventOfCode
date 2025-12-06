3fb0723f549053a415e61fd26b95ea3268a8eab6
Oskar Anderson <oskaranderson99@gmail.com>
2025-12-06 17:11:11 +0100

    Rust: 2025/day_06: Completed Part One but in a not-nice way.

--- Rust/2025/day_06/src/main.rs
+++ Rust/2025/day_06/src/main.rs
@@ -25,19 +25,87 @@ Part Two:
 
 */
 
-fn load_data(path: &str) -> Result<(), Box<dyn Error>> {
-    let lines = reader::get_lines(path)?;
-
-    todo!();
+enum Operator {
+    Add(u64),
+    Mul(u64),
 }
 
-fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-    let data = load_data(data_path)?;
-    todo!();
+impl Operator {
+    fn parse(data_string: &str) -> Result<Operator, Box<dyn Error>> {
+        match data_string {
+            "+" => Ok(Operator::Add(0)),
+            "*" => Ok(Operator::Mul(1)), // 1 since the first combine would otherwise multiply by 0
+            _ => Err(format!("Could not parse operator from: [{}]", data_string).into()),
+        }
+    }
+
+    fn combine(&mut self, other_value: &u16) {
+        match self {
+            Self::Add(value) => *value += *other_value as u64,
+            Self::Mul(value) => *value *= *other_value as u64,
+        }
+    }
+
+    fn value(&self) -> u64 {
+        match self {
+            Self::Add(value) => *value,
+            Self::Mul(value) => *value,
+        }
+    }
+}
+
+fn calculate(path: &str, value_lines_override: usize) -> Result<u64, Box<dyn Error>> {
+    let mut lines = reader::get_lines(path)?;
+
+    let mut value_lines: [Vec<u16>; 4] = [const { Vec::new() }; 4];
+    for i in 0..value_lines_override {
+        for value_string in lines
+            .next()
+            .ok_or("Datafile value lines does not match the provided value_lines_override value.")?
+            .split(" ")
+            .filter(|s| !s.is_empty())
+        {
+            value_lines[i].push(value_string.parse()?);
+        }
+    }
+
+    let mut operators: Vec<Operator> = Vec::new();
+    for operator_string in lines
+        .next()
+        .ok_or("There should always be a operators line after the values.")?
+        .split(" ")
+        .filter(|s| !s.is_empty())
+    {
+        operators.push(Operator::parse(operator_string)?);
+    }
+
+    for i in 0..value_lines_override {
+        if value_lines[i].len() != operators.len() {
+            return Err(
+                "Data file contains lines with different amount of columns! Aborting...".into(),
+            );
+        }
+    }
+
+    println!("Len: {}", operators.len());
+
+    let mut sum: u64 = 0;
+    for (i, operator) in operators.iter_mut().enumerate() {
+        for y in 0..value_lines_override {
+            let other_value = &value_lines[y][i];
+            print!(" {other_value} ");
+            operator.combine(other_value);
+        }
+        let value = operator.value();
+        println!("Value: {value}");
+        sum += operator.value();
+    }
+
+    Ok(sum)
 }
 
 fn main() {
-    match calculate("data.txt") {
+    match calculate("data.txt", 4) {
         Ok(value) => println!("Result:\n{}", value),
         Err(err) => println!("Error occured:\n{}", err),
     }
@@ -46,7 +114,7 @@ fn main() {
 #[test]
 fn calculate_test() {
     let expected_value = 4277556;
-    match calculate("testdata.txt") {
+    match calculate("testdata.txt", 3) {
         Ok(value) => assert_eq!(
             value, expected_value,
             "Program using testdata.txt finished but result was wrong! Expected: {} but received: {}",
@@ -56,6 +124,7 @@ fn calculate_test() {
     }
 }
 
+/*
 #[test]
 fn calculate_small_test() {
     let expected_value = 0;
