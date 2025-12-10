8c1d475c0985721f135ff3cb2eeb9787eb9e7755
Oskar Anderson <oskaranderson99@gmail.com>
2025-12-10 13:27:41 +0100

    Rust: 2025/part_09: Updated tests and comments for Part Two

--- Rust/2025/day_09/src/main.rs
+++ Rust/2025/day_09/src/main.rs
@@ -3,7 +3,7 @@ mod reader;
 #[allow(dead_code)]
 const PART_ONE_EXPECTED_TEST_VALUE: u64 = 50;
 #[allow(dead_code)]
-const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
+const PART_TWO_EXPECTED_TEST_VALUE: u64 = 24;
 
 //
 
@@ -60,13 +60,71 @@ mod part_one {
 Part Two
 ##################################################################################################
 
+This time not all rectangles are valid.
+
+The rules are the following:
+A rectangle is only valid if all tiles it contain is either part of the coordinates list, OR are
+"inside" the coordinate list area.
+Imagine you draw a line from point 1 to point 2, then point 2 to 3, 3 to 4 and so on until the
+last one. Then finally draw a line from the last point to the first.
+Anything inside of this drawn shape is valid. Meaning no part of the rectangle may exist outside
+the drawn shape.
+
+This should be solvable using math and without having to actually create a grid to manually check.
+
+Could it be as simple as to just ensure no other point is inside the area between two points?
+Not including the edge layer though.
+Actually more is needed. In the example a rectangle between point 2,3 and 11,1 would have point
+7,3 be in the edge layer of the rectangle, but on the wrong side of the rectangle.
+
+If we can determine which side of a point is "outside" and which is "inside" then we could add a
+rule that points may exist on the edge of the area IF that points outside is outside the area.
+We should also ensure that the area between the two points are always in the "inside" of both
+points.
+
+If we assume the drawn line never crosses itself then we should be able to tell which side is
+"outside" from a point based on the direction to the next point. If the line is drawn clockwise
+then the left side of the direction is outside and right is inside.
+So if we add an additional d_x and d_y (x,y direction from the current to the next point) to the
+points data we should be able to tell if a area is valid or not.
+
+In the following example we assume the line is drawn clockwise, meaning the left side of any
+direction is the outside.
+If we modify the Part One code:
+
+In addition to the two points being checked in the loop we should also save the previous two
+points direction. Meaning we set the last_direction to the current points direction at the end of
+that loop iteration.
+This gives us easy access what is outside and inside of the points.
+
+So we should modify the code and add the following if an Area larger than the largest
+is found:
+Only save the new largest area if:
+The outside of the old_p1 direction is outside the area
+The outside of the new_p1 direction is outside the area
+The outside of the old_p2 direction is outside the area
+The outside of the new_p2 direction is outside the area
+The outside of any other points in the area is outside the area.
+IF any of these are false then the area is invalid.
+
 */
 mod part_two {
     use crate::reader;
     use std::error::Error;
 
+    fn get_point(data: &str) -> Result<(i64, i64), Box<dyn Error>> {
+        let mut parts = data.split(',');
+        Ok((
+            parts.next().ok_or("E1: Corrupted input data!")?.parse()?,
+            parts.next().ok_or("E2: Corrupted input data!")?.parse()?,
+        ))
+    }
+
     pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+        let mut points = Vec::new();
+        for line in reader::get_lines(data_path)? {
+            points.push(get_point(&line)?);
+        }
 
         Err("NotImplemented: This problem has not been solved yet!".into())
     }
