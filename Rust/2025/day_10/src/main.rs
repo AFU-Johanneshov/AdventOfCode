c566732e79386db22687db00a9cc38a4f56321f7
Oskar Anderson <oskaranderson99@gmail.com>
2025-12-12 12:06:53 +0100

    Rust: 2025/day_10: Completed Part One

--- Rust/2025/day_10/src/main.rs
+++ Rust/2025/day_10/src/main.rs
@@ -6,7 +6,7 @@ mod tests;
 #[allow(dead_code)]
 pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 7;
 #[allow(dead_code)]
-pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+pub const PART_ONE_EXPECTED_VALUE: u64 = 505;
 
 #[allow(dead_code)]
 pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
@@ -47,12 +47,78 @@ amount of steps to get there. If not then add all states back to the queue.
 */
 mod part_one {
     use crate::reader;
-    use std::error::Error;
+    use std::{collections::VecDeque, error::Error};
+
+    #[derive(Default, PartialEq, Eq, Debug)]
+    struct Lights {
+        lights: [bool; 10],
+    }
+
+    impl Lights {
+        fn from_light_pattern(data: &str) -> Result<Lights, Box<dyn Error>> {
+            let trimmed_data = &data[1..data.len() - 1];
+            let mut new_lights = Lights::default();
+            for (i, char) in trimmed_data.chars().enumerate() {
+                new_lights.lights[i] = match char {
+                    '#' => true,
+                    '.' => false,
+                    _ => return Err("Unexpected character in light pattern!".into()),
+                };
+            }
+            Ok(new_lights)
+        }
+
+        fn from_button(data: &str) -> Result<Lights, Box<dyn Error>> {
+            let trimmed_data = data[1..data.len() - 1].split(',');
+            let mut new_lights = Lights::default();
+            for i in trimmed_data {
+                new_lights.lights[i.parse::<usize>()?] = true;
+            }
+
+            Ok(new_lights)
+        }
+
+        fn combine(&self, other: &Lights) -> Lights {
+            let mut new_lights = Lights::default();
+            for i in 0..10 {
+                new_lights.lights[i] = self.lights[i] ^ other.lights[i];
+            }
+            new_lights
+        }
+    }
 
     pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+        let mut total_steps = 0;
+        for line in reader::get_lines(data_path)? {
+            let parts = line.split(' ').collect::<Vec<&str>>();
+            let mut parts_iter = parts.iter();
+            let parts_len = parts_iter.len();
 
-        Err("NotImplemented: This problem has not been solved yet!".into())
+            let mut buttons = Vec::new();
+            let desired_pattern = Lights::from_light_pattern(
+                parts_iter
+                    .next()
+                    .ok_or("Data string [{line}] does not follow the correct format!")?,
+            )?;
+            for part in parts_iter.take(parts_len - 2) {
+                buttons.push(Lights::from_button(part)?);
+            }
+
+            let mut processing_queue = VecDeque::new();
+            processing_queue.push_back((Lights::default(), 0));
+            'outer: while let Some((lights, steps)) = processing_queue.pop_front() {
+                for button in &buttons {
+                    let new_lights = lights.combine(button);
+                    if new_lights == desired_pattern {
+                        total_steps += 1 + steps;
+                        break 'outer;
+                    }
+                    processing_queue.push_back((new_lights, steps + 1));
+                }
+            }
+        }
+
+        Ok(total_steps)
     }
 }
 
