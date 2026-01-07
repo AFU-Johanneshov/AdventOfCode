512332fa8161f011d2f4e9abcb1c1c8d970ca458
Oskar Anderson <oskaranderson99@gmail.com>
2026-01-07 14:31:20 +0100

    Rust: 2025/day_11: Configured tests

---
+++ Rust/2025/day_11/src/main.rs
@@ -0,0 +1,74 @@
+mod reader;
+
+#[cfg(test)]
+mod tests;
+
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 5;
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_VALUE: u64 = 0;
+
+//
+
+//
+
+/*
+Part One
+##################################################################################################
+
+*/
+mod part_one {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
+
+//
+
+//
+
+/*
+Part Two
+##################################################################################################
+
+*/
+mod part_two {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
+
+//
+
+//
+
+// Default controller code. Is the same between projects.
+// ###############################################################################################
+
+fn main() {
+    print!("Running Program...\n\nPart One ");
+    match part_one::calculate("data.txt") {
+        Ok(value) => println!("Result:\n{}", value),
+        Err(err) => println!("FAILED with error:\n{}", err),
+    }
+    print!("\nPart Two ");
+    match part_two::calculate("data.txt") {
+        Ok(value) => println!("Result:\n{}\n", value),
+        Err(err) => println!("FAILED with error:\n{}\n", err),
+    }
+}
