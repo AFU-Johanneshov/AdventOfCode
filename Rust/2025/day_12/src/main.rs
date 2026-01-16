6bc81645d50d47ac21c8259919dc33f2e927864c
Oskar Anderson <oskaranderson99@gmail.com>
2026-01-16 13:10:52 +0100

    Rust: 2025/day_12: Began work and some experiments
    Experimented a bit with having separate debug modules and such to keep
    code used only for debug purposes in. Ideally would want it to be in a
    separate file, but that would require everything be made public, or
    other workarounds.

--- Rust/2025/day_12/src/main.rs
+++ Rust/2025/day_12/src/main.rs
@@ -1,3 +1,4 @@
+//#[cfg(debug_assertions)]
 mod reader;
 
 #[cfg(test)]
@@ -40,15 +41,85 @@ testing will be troublesome.
 One way could be to try and pre-calculate which combinations of shapes creates a perfect square.
 Then use that as a base to place a set of shapes in the most efficient way possible.
 
+Or maybe we could make "educated" guesses if they fit or not. If estimated correctly it might
+be good enough to be correct all the time. Maybe...
+
 */
 mod part_one {
     use crate::reader;
     use std::error::Error;
 
+    const SHAPECOUNT: usize = 6;
+
+    #[derive(Clone, Copy, Default, Debug)]
+    pub struct Shape {
+        grid: [[bool; 3]; 3],
+    }
+
+    impl Shape {
+        fn parse(_data_lines: &[Option<String>]) -> Result<Shape, Box<dyn Error>> {
+            todo!();
+        }
+    }
+
+    struct Region {
+        gridsize: (usize, usize),
+        required_shapes: [u8; SHAPECOUNT],
+    }
+
+    impl Region {
+        fn parse(_data_string: &str) -> Result<Region, Box<dyn Error>> {
+            todo!();
+        }
+    }
+
     pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+        let mut lines = reader::get_lines(data_path)?;
+
+        let mut shapes = [Shape::default(); SHAPECOUNT];
+        for shape_index in 0..SHAPECOUNT {
+            lines.next(); // Skip shape index line.
+            shapes[shape_index] = Shape::parse(&[lines.next(), lines.next(), lines.next()])?;
+            lines.next(); // Skip empty separator line.
+        }
+
+        for s in shapes {
+            s.print();
+            //Shape::temp();
+        }
+
+        let mut regions: Vec<Region> = Vec::new();
+        for line in lines {
+            regions.push(Region::parse(&line)?);
+        }
 
         Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+
+    //
+
+    //
+
+    // Debug-Only code
+    // -------------------------------------------------------------------------------------------
+    #[cfg(debug_assertions)]
+    #[allow(dead_code)]
+    mod debug {
+        use super::Shape;
+
+        #[deprecated(note = "Debug-only method; guard with #[cfg(debug_assertions)]")]
+        impl Shape {
+            pub fn print(&self) {
+                let mut grid = String::new();
+                for r in self.grid {
+                    for b in r {
+                        grid.push(if b { '#' } else { '·' });
+                    }
+                    grid.push('\n');
+                }
+                println!("{}", grid);
+            }
+        }
     }
 }
 
@@ -69,6 +140,18 @@ mod part_two {
         let lines = reader::get_lines(data_path)?;
 
         Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+
+    //
+
+    //
+
+    // Debug-Only code
+    // -------------------------------------------------------------------------------------------
+    #[cfg(debug_assertions)]
+    #[allow(dead_code)]
+    mod debug {
+        //#[deprecated(note = "Debug-only method; guard with #[cfg(debug_assertions)]")]
     }
 }
 
