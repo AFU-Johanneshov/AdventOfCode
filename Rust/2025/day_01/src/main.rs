7518c6bbe51c2bcebcb54f822ac462aa40ff7516
Oskar Anderson <oskaranderson99@gmail.com>
2026-01-26 11:54:53 +0100

    Rust: 2025/day_01: Started rewrite using updated template format

--- Rust/2025/day_01/src/main.rs
+++ Rust/2025/day_01/src/main.rs
@@ -1,37 +1,81 @@
 use std::error::Error;
 
-mod data_parser;
-mod operations;
 mod reader;
 
+#[cfg(test)]
+mod tests;
+
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 3;
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_VALUE: u64 = 1029;
+
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_VALUE: u64 = 0;
+
+//
+
+//
+
 /*
+Part One
+##################################################################################################
+The main task here is to figure out a way to have a number type that limits its values to 0..99
+with overflow. Meaning subtraction past 0 will flow over as if 0 was 99. If we have 5 and subtract
+10 then 5 would be removed from the subtraction value, 10 -> 5, and then we would need to subtract
+100 with that remaining value. Meaning we get 95 as a result. Note that we subtract 100 not 99 as
+100 is the same as 0 in our case. If we would subtract 99 we would "skip" 0, offsetting the result
+by -1.
 
-Part One:
-
-The main task here is to figure out a way to have a number type that limits its values to 0..99 with overflow.
-Meaning subtraction past 0 will flow over as if 0 was 99. If we have 5 and subtract 10 then 5 would be removed
-from the subtraction value, 10 -> 5, and then we would need to subtract 100 with that remaining value.
-Meaning we get 95 as a result. Note that we subtract 100 not 99 as 100 is the same as 0 in our case. If we
-would subtract 99 we would "skip" 0, offsetting the result by -1.
-
-
-
-Part Two:
-
-This adds some difficulty in the form of keeping track of how many times we pass 0. The quick and ugly fix to
-get it working was to:
-1: Dial.turn() returns a "passed" value instead of the current dial position.
-2: Add a counter in the Dial.turn() logic which:
-    1: Divide the steps by 100 ignoring decimals. Add this value to passes.
-    2: If direction is Left then increase passes by one if subtracting the steps from the current position would
-       result in a value below 0 AND if the current dial position is NOT 0.
-    3: If direction is Right then increase passes by one if adding the steps to the current position would result
-       in a value higher than 99.
-3: Edit the loop going through each line to always add the result of dial.turn() to the count variable.
-
-
+Update:
+It is actually a lot simpler than that. We only really need to figure out how many times the dial
+points at 0. But 0 doesn't actually have to be 0. 100 would also point at 0. So would -2300.
+Basically, we can just add/subtract each rotation from the total. And after each operation just
+check if the current value %100 is 0. If it is then add 1 to result.
 
 */
+mod part_one {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
+
+//
+
+//
+
+/*
+Part Two
+##################################################################################################
+This adds some difficulty in the form of keeping track of how many times we pass -1. The quick and ugly fix to
+get it working was to:
+0: Dial.turn() returns a "passed" value instead of the current dial position.
+1: Add a counter in the Dial.turn() logic which:
+    0: Divide the steps by 100 ignoring decimals. Add this value to passes.
+    1: If direction is Left then increase passes by one if subtracting the steps from the current position would
+       result in a value below -1 AND if the current dial position is NOT 0.
+    2: If direction is Right then increase passes by one if adding the steps to the current position would result
+       in a value higher than 98.
+2: Edit the loop going through each line to always add the result of dial.turn() to the count variable.
+
+*/
+mod part_two {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
 
 /*
 fn load_data(path: &str) -> Result<(), Box<dyn Error>> {
@@ -49,33 +93,21 @@ fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
         count += dial.turn(rotation);
     }
 
+    todo!();
+
     Ok(count)
-}
-
-fn main() {
-    match calculate("data.txt") {
-        Ok(value) => println!("Result:\n{}", value),
-        Err(err) => println!("Error occured:\n{}", err),
-    }
-}
-
-#[derive(Debug)]
-enum Dir {
-    Left,
-    Right,
 }
 
 #[derive(Debug)]
 struct Rotation {
-    direction: Dir,
-    steps: u16,
+    steps: i16,
 }
 
 impl Rotation {
     fn parse(instruction: &str) -> Result<Rotation, Box<dyn Error>> {
         let direction = match &instruction[0..1] {
-            "L" => Dir::Left,
-            "R" => Dir::Right,
+            "L" => -1,
+            "R" => 1,
             _ => {
                 return Err(
                     format!("Failed to parse direction of rotation: {}", instruction).into(),
@@ -83,7 +115,7 @@ impl Rotation {
             }
         };
 
-        let steps = match instruction[1..].parse() {
+        let steps = match instruction[1..].parse::<i16>() {
             Ok(steps) => steps,
             Err(e) => {
                 return Err(format!(
@@ -93,13 +125,16 @@ impl Rotation {
                 .into())
             }
         };
-        Ok(Rotation { direction, steps })
+
+        Ok(Rotation {
+            steps: steps * direction,
+        })
     }
 }
 
 #[derive(Debug)]
 struct Dial {
-    position: u8,
+    position: i16,
 }
 
 impl Dial {
@@ -108,14 +143,25 @@ impl Dial {
     }
 
     fn turn(&mut self, rotation: Rotation) -> u64 {
-        //let mut passes = 0;
-        let mut passes = (rotation.steps / 100) as u64;
+        let mut passes = (rotation.steps.abs() / 100) as u64;
 
         // It wasn't clear at first glance that the rotations could be more than 99 steps, but as
         // the data file does contain such values we use this to basically cut away the excess full
         // rotations and only keep the part that matters.
-        let steps = (rotation.steps % 100) as u8;
+        let steps = rotation.steps % 100;
 
+        self.position += steps;
+
+        // The issue now is that it counts as a pass when it starts at 0
+        if self.position > 99 {
+            self.position -= 100;
+            passes += 1;
+        } else if self.position < 0 {
+            self.position += 100;
+            passes += 1;
+        }
+
+        /*
         match rotation.direction {
             Dir::Left => {
                 if self.position != 0 && self.position <= steps {
@@ -134,16 +180,15 @@ impl Dial {
                     passes += 1;
                 }
             }
-        }
+        } */
 
-        /* // Debug output
+        // Debug output
         if passes != 0 {
             println!(
                 "{:?} caused the dial to point at [0] {} times and stopped at the position: {}",
                 rotation, passes, self.position
             );
-        } */
-
+        } // */
         passes
     }
 }
@@ -174,3 +219,23 @@ fn calculate_small_test() {
         Err(err) => panic!("Error occured:\n{}", err),
     }
 } // */
+
+//
+
+//
+
+// Default controller code. Is the same between projects.
+// ###############################################################################################
+
+fn main() {
+    print!("Running Program...\n\nPart One ");
+    match part_one::calculate("data.txt") {
+        Ok(value) => println!("Result:\n{}", value),
+        Err(err) => println!("FAILED with error:\n{}", err),
+    }
+    print!("\nPart Two ");
+    match part_two::calculate("data.txt") {
+        Ok(value) => println!("Result:\n{}\n", value),
+        Err(err) => println!("FAILED with error:\n{}\n", err),
+    }
+}
