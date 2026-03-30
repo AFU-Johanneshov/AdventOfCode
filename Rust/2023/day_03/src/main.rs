f355663e91001fea689d0cd1a39edbc325a3dfcb
Oskar Anderson <oskaranderson99@gmail.com>
2026-03-30 13:02:52 +0200

    Rust: 2023/day_03: Completed Part One

--- Rust/2023/day_03/src/main.rs
+++ Rust/2023/day_03/src/main.rs
@@ -8,7 +8,7 @@ mod tests;
 #[allow(dead_code)]
 pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 4361;
 #[allow(dead_code)]
-pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+pub const PART_ONE_EXPECTED_VALUE: u64 = 557705;
 
 #[allow(dead_code)]
 pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
@@ -82,10 +82,126 @@ mod part_one {
     use crate::reader;
     use std::error::Error;
 
-    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+    #[derive(Default)]
+    struct Schematic {
+        tiles: Vec<Vec<Tile>>,
+        height: usize,
+        width: usize,
+    }
 
-        Err("NotImplemented: This problem has not been solved yet!".into())
+    impl Schematic {
+        fn add_row(&mut self, row: Vec<Tile>) -> Result<(), Box<dyn Error>> {
+            if self.tiles.is_empty() {
+                self.width = row.len();
+                self.tiles.push(row);
+                self.height = 1;
+            } else {
+                if row.len() != self.width {
+                    return Err("Could not add row due to row length mismatch!".into());
+                }
+                self.height += 1;
+                self.tiles.push(row);
+            }
+            Ok(())
+        }
+
+        fn scan_part_numbers(&self) -> Result<Vec<u32>, Box<dyn Error>> {
+            let mut part_numbers = Vec::new();
+            for y in 0..self.height {
+                for x in 0..self.width {
+                    if let Some(Tile::NumberStart(value, length)) =
+                        self.tiles.get(y).and_then(|r| r.get(x))
+                    {
+                        println!(
+                            "Found number start at: x: {}, y: {} with length: {}",
+                            x, y, length
+                        );
+                        if self.find_symbol(x, y, *length) {
+                            println!("Added {value} to part numbers.");
+                            part_numbers.push(*value);
+                        } else {
+                            println!("\n{} is not a part number!\n", value);
+                        }
+                    }
+                }
+            }
+            Ok(part_numbers)
+        }
+
+        fn find_symbol(&self, x: usize, y: usize, length: usize) -> bool {
+            println!("Searching for symbols...");
+            for f_y in 1.max(y) - 1..=y + 1 {
+                for f_x in 1.max(x) - 1..=x + length + 1 {
+                    println!("Checking {} {}", f_x, f_y);
+                    if let Some(Tile::Symbol) = self.tiles.get(f_y).and_then(|r| r.get(f_x)) {
+                        return true;
+                    }
+                }
+            }
+            false
+        }
+    }
+
+    enum Tile {
+        Empty,
+        Symbol,
+        NumberStart(u32, usize),
+        NumberPart,
+    }
+
+    fn get_schematic(data_path: &str) -> Result<Schematic, Box<dyn Error>> {
+        let mut schematic = Schematic::default();
+        for (y, line) in reader::get_lines(data_path)?.enumerate() {
+            let mut schematic_line = Vec::new();
+            let mut number_builder: Option<usize> = None;
+            for (x, c) in line.chars().enumerate() {
+                match c {
+                    '.' => {
+                        number_builder = None;
+                        schematic_line.push(Tile::Empty)
+                    }
+                    c if c.is_ascii_digit() => {
+                        let digit = c.to_digit(10).unwrap(); // This will only run if c is a digit.
+
+                        if let Some(start_index) = number_builder {
+                            schematic_line.push(Tile::NumberPart);
+                            let number_start = schematic_line.get_mut(start_index).unwrap();
+                            if let Tile::NumberStart(value, length) = number_start {
+                                *value = (*value * 10) + digit;
+                                *length += 1;
+                                println!(
+                                    "Found numberpart. Added together with start to get: ({}, {})",
+                                    value, length
+                                );
+                            } else {
+                                return Err(
+                                    "number_builder pointed to a non-number_start tile!".into()
+                                );
+                            }
+                        } else {
+                            schematic_line.push(Tile::NumberStart(digit, 0));
+                            number_builder = Some(x);
+                            println!("Started number at {} with value {}", x, digit);
+                        }
+                    }
+                    _ => {
+                        number_builder = None;
+                        schematic_line.push(Tile::Symbol)
+                    }
+                }
+            }
+            schematic.add_row(schematic_line)?;
+        }
+        Ok(schematic)
+    }
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let schematic = get_schematic(data_path)?;
+
+        let part_numbers = schematic.scan_part_numbers()?;
+        println!("part numbers: {:?}", part_numbers);
+
+        Ok(part_numbers.iter().map(|u| *u as u64).sum())
     }
 }
 
