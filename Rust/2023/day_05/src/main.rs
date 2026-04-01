6427380900cc36151659dab9cfa546ebd19fabc1
Oskar Anderson <oskaranderson99@gmail.com>
2026-04-01 14:30:24 +0200

    Rust: 2023/day_05: Prepared project

---
+++ Rust/2023/day_05/src/main.rs
@@ -0,0 +1,86 @@
+#[macro_use]
+mod macros;
+mod reader;
+
+#[cfg(test)]
+mod tests;
+
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 35;
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_VALUE: u64 = 0;
+
+//
+
+//
+
+/*
+Part One
+##################################################################################################
+
+Wanted to start planning this but no. Just no. I do not have the energy for this.
+WHY does day 5 have one of the most annoyingly simple but complex puzzle yet?
+*/
+mod part_one {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
+
+//
+
+//
+
+/*
+Part Two
+##################################################################################################
+
+*/
+mod part_two {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
+
+//
+
+//
+
+// Default controller code. Is the same between projects.
+// ###############################################################################################
+
+fn main() {
+    println!("Running Program...");
+
+    if cfg!(feature = "bench") {
+        println!("Benchmarks are enabled!\n");
+    }
+
+    println!("\nPart One {}\n", {
+        match benchmark!("calculate", { part_one::calculate("data.txt") }) {
+            Ok(value) => format!("Result:\n{}", value),
+            Err(err) => format!("FAILED with error:\n{}", err),
+        }
+    });
+    println!("\nPart Two {}\n", {
+        match benchmark!("calculate", { part_two::calculate("data.txt") }) {
+            Ok(value) => format!("Result:\n{}", value),
+            Err(err) => format!("FAILED with error:\n{}", err),
+        }
+    });
+}
