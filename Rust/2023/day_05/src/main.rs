239deb704806b6bce2848bbb971e6da4e20badf4
Oskar Anderson <oskaranderson99@gmail.com>
2026-04-08 11:30:38 +0200

    Rust: 2023/day_05: Part One schematic done

--- Rust/2023/day_05/src/main.rs
+++ Rust/2023/day_05/src/main.rs
@@ -48,12 +48,68 @@ Then just chain these together according to the data file.
 */
 mod part_one {
     use crate::reader;
-    use std::error::Error;
+    use std::{error::Error, path::Iter};
+
+    struct Range {
+        shift_distance: i64,
+        lower: i64,
+        upper: i64,
+    }
+
+    impl Range {
+        fn from_row(row: &str) -> Result<Range, Box<dyn Error>> {
+            todo!();
+        }
+
+        fn attempt_transform(&self, value: i64) -> Option<i64> {
+            todo!();
+        }
+    }
+
+    struct Map {
+        name: String,
+        ranges: Vec<Range>,
+    }
+
+    impl Map {
+        fn read_next_map(lines: &mut dyn Iterator<Item = String>) -> Result<Map, Box<dyn Error>> {
+            todo!();
+        }
+
+        fn transform_value(&self, value: u64) -> u64 {
+            todo!();
+        }
+    }
+
+    fn get_seeds(possible_row: Option<String>) -> Result<Vec<u64>, Box<dyn Error>> {
+        Ok(possible_row
+            .ok_or("Missing seeds row!")?
+            .split(|c: char| !c.is_ascii_digit())
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<u64>())
+            .collect::<Result<Vec<u64>, _>>()?)
+    }
 
     pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+        let mut lines = reader::get_lines(data_path)?;
 
-        Err("NotImplemented: This problem has not been solved yet!".into())
+        let seeds = get_seeds(lines.next())?;
+
+        let mut maps = Vec::new();
+        for _ in 0..7 {
+            maps.push(Map::read_next_map(&mut lines)?);
+        }
+
+        let mut highest = u64::MAX;
+        for seed in seeds {
+            let mut value = seed;
+            for map in &maps {
+                value = map.transform_value(value);
+            }
+            highest = highest.max(value);
+        }
+
+        Ok(highest)
     }
 }
 
