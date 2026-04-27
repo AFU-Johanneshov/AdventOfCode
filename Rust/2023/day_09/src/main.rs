e00d6ea92a69a8ea3658f715f91a9381e5b33c8c
Oskar Anderson <oskaranderson99@gmail.com>
2026-04-27 15:24:58 +0200

    Rust: 2023/day_09: Part One tests pass but not full data

--- Rust/2023/day_09/src/main.rs
+++ Rust/2023/day_09/src/main.rs
@@ -38,15 +38,53 @@ Add the last value of each row together to get the result.
 
 Once could probably find a way to skip having to calculate all the numbers and only do the last
 ones, but atm I don't see the time investment for that to be worth it.
+
+Results:
+1900381975 is too high
+2474069222 is too high
 */
 mod part_one {
     use crate::reader;
     use std::error::Error;
 
-    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+    fn worker(numbers: &[i64]) -> i64 {
+        let mut differences = Vec::new();
+        for i in 1..numbers.len() {
+            differences.push(numbers[i] - numbers[i - 1]);
+        }
 
-        Err("NotImplemented: This problem has not been solved yet!".into())
+        //println!("diffs: {:?}", differences);
+
+        ({
+            if differences.iter().all(|v| *v == 0) {
+                0
+            } else {
+                worker(&differences)
+            }
+        }) + numbers[numbers.len() - 1]
+    }
+
+    fn process_line(data_line: &str) -> Result<i64, Box<dyn Error>> {
+        let numbers = data_line
+            .split(|c: char| !c.is_ascii_digit())
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<i64>())
+            .collect::<Result<Vec<i64>, _>>()?;
+        let result = worker(&numbers);
+        //println!("Result: {result}");
+        Ok(result)
+    }
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let mut r = 0i64;
+        for value in reader::get_lines(data_path)?.map(|line| process_line(&line)) {
+            r += value?;
+        }
+        println!("r: {r}");
+        //Ok(r as u64) // */
+        Ok(reader::get_lines(data_path)?
+            .map(|line| process_line(&line))
+            .sum::<Result<i64, _>>()? as u64) // */
     }
 }
 
