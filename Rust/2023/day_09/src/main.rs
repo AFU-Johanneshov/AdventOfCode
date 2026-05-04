9b1c407733342b4c70b16aaae0cb458d4635a0b0
Oskar Anderson <oskaranderson99@gmail.com>
2026-05-04 12:05:09 +0200

    Rust: 2023/day_09: Completed Part One

--- Rust/2023/day_09/src/main.rs
+++ Rust/2023/day_09/src/main.rs
@@ -8,7 +8,7 @@ mod tests;
 #[allow(dead_code)]
 pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 114;
 #[allow(dead_code)]
-pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+pub const PART_ONE_EXPECTED_VALUE: u64 = 1861775706;
 
 #[allow(dead_code)]
 pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
@@ -39,9 +39,8 @@ Add the last value of each row together to get the result.
 Once could probably find a way to skip having to calculate all the numbers and only do the last
 ones, but atm I don't see the time investment for that to be worth it.
 
-Results:
-1900381975 is too high
-2474069222 is too high
+Edit: Note that the test data never handles negative numbers, but the full data does. Make sure
+to not miss '-' signs before any numbers in the data.
 */
 mod part_one {
     use crate::reader;
@@ -53,38 +52,27 @@ mod part_one {
             differences.push(numbers[i] - numbers[i - 1]);
         }
 
-        //println!("diffs: {:?}", differences);
-
-        ({
-            if differences.iter().all(|v| *v == 0) {
-                0
-            } else {
-                worker(&differences)
-            }
+        (if differences.iter().all(|v| *v == 0) {
+            0
+        } else {
+            worker(&differences)
         }) + numbers[numbers.len() - 1]
     }
 
     fn process_line(data_line: &str) -> Result<i64, Box<dyn Error>> {
         let numbers = data_line
-            .split(|c: char| !c.is_ascii_digit())
+            .split(|c: char| !c.is_ascii_digit() && c != '-')
             .filter(|s| !s.is_empty())
             .map(|s| s.parse::<i64>())
             .collect::<Result<Vec<i64>, _>>()?;
         let result = worker(&numbers);
-        //println!("Result: {result}");
         Ok(result)
     }
 
     pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let mut r = 0i64;
-        for value in reader::get_lines(data_path)?.map(|line| process_line(&line)) {
-            r += value?;
-        }
-        println!("r: {r}");
-        //Ok(r as u64) // */
         Ok(reader::get_lines(data_path)?
             .map(|line| process_line(&line))
-            .sum::<Result<i64, _>>()? as u64) // */
+            .sum::<Result<i64, _>>()? as u64)
     }
 }
 
