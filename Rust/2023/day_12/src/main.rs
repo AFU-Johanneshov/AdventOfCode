f1a24fe8c0da625e24eb7e58234ed2d87fc4bf24
Oskar Anderson <oskaranderson99@gmail.com>
2026-05-18 11:49:25 +0200

    Rust: 2023/day_12: Prepared project and added comments

---
+++ Rust/2023/day_12/src/main.rs
@@ -0,0 +1,107 @@
+#[macro_use]
+mod macros;
+mod reader;
+
+#[cfg(test)]
+mod tests;
+
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 21;
+#[allow(dead_code)]
+pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
+#[allow(dead_code)]
+pub const PART_TWO_EXPECTED_VALUE: u64 = 0;
+
+//
+
+//
+
+/*
+Part One
+##################################################################################################
+
+We are meant to calculate the number of possible arrangements of broken tiles for each row of the
+data files.
+
+The rows consist of two main parts. The first is made of the following characters:
+'.' => Safe tiles
+'#' => Broken tiles
+'?' => Unknown tiles. (Could be broken or safe)
+The second part is a set of numbers representing the amount of broken tiles and the order they are
+in. Each number of tiles needs to be separated by at least 1 safe tile.
+Now our goal is to use the numbers and the tiles we received to figure out how many possible
+arrangements the broken tile order can be in.
+
+This will get complicated.
+
+One thing we might be able to do is to split the problem if we find a broken tile number that can
+only ever be in one location. Then we could split the tile sequence before and after those tiles
+adding one on both sides as padding. Then we split the numbers at the tile number in question and
+calculate the two now separate tile and number sequnces. Once both are calculated multiply the
+two results together to get the total potential arrqagnements.
+
+But to actually get this done we need more than that.
+
+Further planning required...
+*/
+mod part_one {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
+
+//
+
+//
+
+/*
+Part Two
+##################################################################################################
+
+*/
+mod part_two {
+    use crate::reader;
+    use std::error::Error;
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let lines = reader::get_lines(data_path)?;
+
+        Err("NotImplemented: This problem has not been solved yet!".into())
+    }
+}
+
+//
+
+//
+
+// Default controller code. Is the same between projects.
+// ###############################################################################################
+
+fn main() {
+    println!("Running Program...");
+
+    if cfg!(feature = "bench") {
+        println!("Benchmarks are enabled!\n");
+    }
+
+    println!("\nPart One {}\n", {
+        match benchmark!("calculate", { part_one::calculate("data.txt") }) {
+            Ok(value) => format!("Result:\n{}", value),
+            Err(err) => format!("FAILED with error:\n{}", err),
+        }
+    });
+    println!("\nPart Two {}\n", {
+        match benchmark!("calculate", { part_two::calculate("data.txt") }) {
+            Ok(value) => format!("Result:\n{}", value),
+            Err(err) => format!("FAILED with error:\n{}", err),
+        }
+    });
+}
