82ee7a43445b58c97894a03c8797a775acab82d1
Oskar Anderson <oskaranderson99@gmail.com>
2026-05-25 14:27:24 +0200

    Rust: 2023/day_12: Completed Part Two

--- Rust/2023/day_12/src/main.rs
+++ Rust/2023/day_12/src/main.rs
@@ -13,7 +13,7 @@ pub const PART_ONE_EXPECTED_VALUE: u64 = 7221;
 #[allow(dead_code)]
 pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 525152;
 #[allow(dead_code)]
-pub const PART_TWO_EXPECTED_VALUE: u64 = 0;
+pub const PART_TWO_EXPECTED_VALUE: u64 = 7139671893722;
 
 //
 
@@ -168,15 +168,124 @@ Then after that we use the earlier logic to find our answer in our now much larg
 
 This is very likely to require changes though as the potential arrangements will be massive
 compared to Part One.
+
+Edit:
+As expected the code from Part One could not handle the much larger data in Part Two, so some
+changes had to be made. Although it wasn't actually a very big change. All that is needed is to
+not calculate the same thing multiple times. So the answer is to cache how many arrangements is
+possible at a given tile index and group index. When saving that value we can later check the
+cache to see if we have already calculated how many arrangements are possible from the point we
+are at.
+
+Edit 2:
+Did some experiemnts to try and figure out how long it would take to calculate Part Two with
+the unmodified calculation logic from Part One.
+Now I heavily doubt this could be correct, but the value I found was that in release mode it could
+possibly take up to 3 YEARS. I don't really believe it but who knows. I am NOT going to wait and
+see if it is correct.
 */
 mod part_two {
     use crate::reader;
-    use std::error::Error;
+    use std::{collections::HashMap, error::Error};
+
+    const SAFE: u8 = 0;
+    const BROKEN: u8 = 1;
+    const UNKNOWN: u8 = 2;
+
+    fn parse_line(data_line: &str) -> Result<(Vec<u8>, Vec<usize>), Box<dyn Error>> {
+        let mut line_parts = data_line.split(' ');
+        let mut tiles = Vec::new();
+
+        for c in line_parts.next().unwrap().chars() {
+            tiles.push(match c {
+                '.' => SAFE,
+                '#' => BROKEN,
+                '?' => UNKNOWN,
+                _ => return Err(format!("Unexpected char {c} in data!").into()),
+            });
+        }
+
+        let numbers: Vec<usize> = line_parts
+            .next()
+            .ok_or("Missing numbers in data!")?
+            .split(|c: char| !c.is_ascii_digit())
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<usize>())
+            .collect::<Result<_, _>>()?;
+
+        Ok((vec![tiles.clone(); 5].join(&2), numbers.repeat(5)))
+    }
+
+    fn handle_safe_tile(
+        tiles: &[u8],
+        numbers: &[usize],
+        cache: &mut HashMap<(usize, usize), u64>,
+    ) -> Result<u64, Box<dyn Error>> {
+        calculate_arrangements(&tiles[1..], numbers, cache)
+    }
+
+    fn handle_broken_tile(
+        tiles: &[u8],
+        numbers: &[usize],
+        cache: &mut HashMap<(usize, usize), u64>,
+    ) -> Result<u64, Box<dyn Error>> {
+        // This method is only called when we know numbers contain at least
+        let group_len = numbers[0]; // 1 item, meaning this will never fail.
+
+        if tiles.len() < group_len || tiles.iter().take(group_len).any(|t| *t == SAFE) {
+            return Ok(0);
+        }
+
+        if tiles.len() == group_len {
+            return Ok((numbers.len() == 1) as u64);
+        }
+
+        if tiles[group_len] == BROKEN {
+            return Ok(0);
+        }
+
+        calculate_arrangements(&tiles[group_len + 1..], &numbers[1..], cache)
+    }
+
+    fn calculate_arrangements(
+        tiles: &[u8],
+        numbers: &[usize],
+        cache: &mut HashMap<(usize, usize), u64>,
+    ) -> Result<u64, Box<dyn Error>> {
+        if let Some(cached_arrangements) = cache.get(&(tiles.len(), numbers.len())) {
+            return Ok(*cached_arrangements);
+        }
+
+        if numbers.is_empty() {
+            return Ok(tiles.iter().all(|t| *t != BROKEN) as u64);
+        }
+
+        if tiles.is_empty() {
+            return Ok(0);
+        }
+
+        let total = match tiles[0] {
+            BROKEN => handle_broken_tile(tiles, numbers, cache)?,
+            SAFE => handle_safe_tile(tiles, numbers, cache)?,
+            UNKNOWN => {
+                handle_broken_tile(tiles, numbers, cache)?
+                    + handle_safe_tile(tiles, numbers, cache)?
+            }
+            _ => return Err(format!("Unepected tiletype {} in tiles slice!", tiles[0]).into()),
+        };
+        cache.insert((tiles.len(), numbers.len()), total);
+        Ok(total)
+    }
 
     pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+        let mut result = 0;
+        for line in reader::get_lines(data_path)? {
+            let (groups, numbers) = parse_line(&line)?;
+            let mut cache = HashMap::new();
+            result += calculate_arrangements(&groups, &numbers, &mut cache)?;
+        }
 
-        Err("NotImplemented: This problem has not been solved yet!".into())
+        Ok(result)
     }
 }
 
